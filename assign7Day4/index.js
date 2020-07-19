6b290ad3b0b979a304129b2296aba3917767f212
Parichay-Sethi <68245981+Parichay-Sethi@users.noreply.github.com>
2020-07-19 21:44:49 +0530

    assignment15

---
+++ assign7Day4/index.js
@@ -0,0 +1,13 @@
+let n = +prompt(`enter the value till wher you want prime numbers`);
+for (var counter = 0; counter <= n; counter++) {
+
+    var notPrime = false;
+    for (var i = 2; i <= counter; i++) {
+        if (counter%i===0 && i!==counter) {
+            notPrime = true;
+        }
+    }
+    if (notPrime === false) {
+                console.log(counter);
+    }
+}
